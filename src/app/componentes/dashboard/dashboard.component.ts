388dea1481b9dde46c462d5a133b1f9792fddb8b
yarethsan <joogalaxy00@gmail.com>
2024-11-06 14:03:04 -0600

    Se agregan estilos a la pantalla de venta

--- src/app/componentes/dashboard/dashboard.component.ts
+++ src/app/componentes/dashboard/dashboard.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterOutlet , Router} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
+  constructor(private router : Router){}
+
+  pagina(ruta:string){
+    this.router.navigate([ruta]);
+  }
 }
