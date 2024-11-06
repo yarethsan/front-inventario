d2903daffd93c714d5fad28a7e2d857e24ec477a
yarethsan <joogalaxy00@gmail.com>
2024-11-06 14:01:25 -0600

    Se agregan componentes hijos a las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './componentes/dashboard/dashboard.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { VentasComponent } from './componentes/ventas/ventas.component';
 
 export const routes: Routes = [
-  { path: "" , component: DashboardComponent
+  { path: "" , component: DashboardComponent,
+    children:[
+      {path: "", redirectTo: "menu", pathMatch:"full"},
+      {path: "menu", component: InicioComponent},
+      {path: "ventas", component: VentasComponent}
+    ]
 
   }
 ];
