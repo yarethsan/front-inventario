faa97ce940bd6e7d84bb5ace74c13d6e3929d76c
yarethsan <joogalaxy00@gmail.com>
2024-11-05 23:12:14 -0600

    Se crea el dashboard inicial de inventario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "" , component: DashboardComponent
+
+  }
+];
