ea2aa26adc3f6b2f916bb1a3748fc7d2605ee362
yarethsan <joogalaxy00@gmail.com>
2024-11-06 16:23:32 -0600

    metodo para la pestaña de venta desde el inicio

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -8,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
-
+  constructor(private router : Router){}
+  pagina(ruta:string){
+    this.router.navigate([ruta]);
+  }
 }
