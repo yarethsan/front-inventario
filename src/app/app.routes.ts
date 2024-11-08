c5416d8f306a28832c7a76b7667c168533bf3916
yarethsan <joogalaxy00@gmail.com>
2024-11-08 11:18:38 -0600

    Ruta de inventarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './componentes/dashboard/dashboard.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { VentasComponent } from './componentes/ventas/ventas.component';
+import { InventarioComponent } from './componentes/inventario/inventario.component';
 
 export const routes: Routes = [
   { path: "" , component: DashboardComponent,
     children:[
       {path: "", redirectTo: "menu", pathMatch:"full"},
       {path: "menu", component: InicioComponent},
-      {path: "ventas", component: VentasComponent}
+      {path: "ventas", component: VentasComponent},
+      {path: "inventario", component: InventarioComponent}
     ]
 
   }
