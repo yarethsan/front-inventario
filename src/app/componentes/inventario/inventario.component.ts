b5cfe81e216e2b78014cb84ac09c5c469134b960
yarethsan <joogalaxy00@gmail.com>
2024-11-09 13:32:47 -0600

    metodo service

--- src/app/componentes/inventario/inventario.component.ts
+++ src/app/componentes/inventario/inventario.component.ts
@@ -1,27 +1,58 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { InventarioService } from '../../services/inventario.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { error } from 'console';
+
+
 
 @Component({
   selector: 'app-inventario',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './inventario.component.html',
   styleUrl: './inventario.component.css'
 })
 export class InventarioComponent {
+
   inventario: any;
-  constructor(private router : Router, private _inventario:InventarioService){}
+  form: FormGroup = new FormGroup({
+    codigoBarras: new FormControl(''),
+    nombreProducto: new FormControl(''),
+    precio: new FormControl(''),
+    cantidad: new FormControl('')
+  })
+  constructor(private router: Router, private _inventario: InventarioService) { }
 
   ngOnInit(): void {
-this._inventario.obtenerInventario().subscribe({
-  next:inventario => this.inventario=inventario, 
-  error: error => console.error('Error:', error),
-})
+this.obtenerProductos()
   }
 
-  pagina(ruta:string){
+  obtenerProductos(){
+    this._inventario.obtenerInventario().subscribe({
+      next: inventario => this.inventario = inventario,
+      error: error => console.error('Error:', error),
+    })
+  }
+
+  pagina(ruta: string) {
     this.router.navigate([ruta]);
+  }
+
+  agregarProducto() {
+    const producto = {
+      idProducto: null,
+      //codigoBarras: this.form.get('codigoBarras')?.value,
+      nombreProducto: this.form.get('nombreProducto')?.value,
+      precio: this.form.get('precio')?.value,
+      cantidadDisponible: this.form.get('cantidad')?.value
+    }
+    this._inventario.nuevoProducto(producto).subscribe({
+      next: respuesta => console.log(respuesta),
+      error: error=> console.error(error),
+      complete: ()=> this.obtenerProductos()
+    })
+    console.log(this.form.value)
   }
 
 }
