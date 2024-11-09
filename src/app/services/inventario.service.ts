97990db9a4325fb2bb3c25d0b35ee29b29f0f739
yarethsan <joogalaxy00@gmail.com>
2024-11-09 12:33:44 -0600

    Servicios de inventarios

--- src/app/services/inventario.service.ts
+++ src/app/services/inventario.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { enviroment } from '../../enviroments/enviroment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InventarioService {
 
-  constructor() { }
+  private URL=`${enviroment.url}/inventario`
+  constructor(private http:HttpClient) { }
+
+  obtenerInventario():Observable<any>{
+    return this.http.get(`${this.URL}/`);
+  }
+
+  nuevoProducto(inventario:any, id:number):Observable<any>{
+    return this.http.post(`${this.URL}/nuevo`,inventario); 
+  }
+  actualizarInventario(inventario:any, id:number):Observable<any>{
+    return this.http.put(`${this.URL}/editar/${inventario}`, inventario);
+  }           
+  eliminarInventario(inventario:any, id:number):Observable<any>{
+    return this.http.delete(`${this.URL}/${inventario}`)
+  }
 }
