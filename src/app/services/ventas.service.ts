32a4e573c95bbade341536d6ce02ea3a7170e9c0
yarethsan <joogalaxy00@gmail.com>
2024-11-09 22:50:40 -0600

    ventas service

---
+++ src/app/services/ventas.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { enviroment } from '../../enviroments/enviroment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VentasService {
+  private URL = `${enviroment.url}/ventas`;
+  constructor(private http: HttpClient) { }
+  agregarVenta(venta: any): Observable<any> {
+    return this.http.post<any>(this.URL, venta);
+  }
+
+}
