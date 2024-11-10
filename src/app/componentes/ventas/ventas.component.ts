482f3a93a6bb8050d0074a5e80a2b7f5e926c655
yarethsan <joogalaxy00@gmail.com>
2024-11-09 22:50:21 -0600

    venta componente

--- src/app/componentes/ventas/ventas.component.ts
+++ src/app/componentes/ventas/ventas.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { VentasService } from '../../services/ventas.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ventas',
@@ -8,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './ventas.component.css'
 })
 export class VentasComponent {
+
+  constructor(private ventasService: VentasService){}
+  ngOnInit(){}
+nuevaVenta(){
+   const  venta = {
+    idVenta: null,
+    fecha: new Date(),
+    total: null,
+    idUsuario: {idUsuario: parseInt("",10)}
+   }
+   this.ventasService.agregarVenta(venta).subscribe({
+    next: respuesta => console.log(), 
+  error: error => console.log()
+})
+}
+
+
 
 }
