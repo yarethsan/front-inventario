afd00a5f09b8f0664634a872f4e13af841e7a5f1
yarethsan <joogalaxy00@gmail.com>
2024-11-09 20:21:34 -0600

    modificaion de service usuario

---
+++ src/app/services/usuarios.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { enviroment } from '../../enviroments/enviroment';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+  private URL = `${enviroment.url}/usuario`;
+  constructor(private http: HttpClient) {}
+
+  obtenerTodosUsuarios(): Observable<any> {
+    return this.http.get(`${this.URL}/`);
+  }
+
+  obtenerUsuarioPorId(id: number): Observable<any> {
+    return this.http.get(`${this.URL}/${id}`);
+  }
+
+  agregarNuevoUsuario(usuario: any): Observable<any> {
+    return this.http.post(`${this.URL}/`, usuario);
+  }
+
+  actualizarUsuario(usuario: any, id: number): Observable<any> {
+    return this.http.put(`${this.URL}/edita/${id}`, usuario);
+  }
+
+  eliminarUsuario(id: number): Observable<any> {
+    return this.http.delete(`${this.URL}/eliminar/${id}`);
+  }
+}
