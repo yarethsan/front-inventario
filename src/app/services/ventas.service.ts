db29ce4d11441caa623be293750ba9ccd34b57fb
yarethsan <joogalaxy00@gmail.com>
2024-11-11 01:38:36 -0600

    se cambia la url del metodo de agregar venta

--- src/app/services/ventas.service.ts
+++ src/app/services/ventas.service.ts
@@ -10,7 +10,7 @@ export class VentasService {
   private URL = `${enviroment.url}/ventas`;
   constructor(private http: HttpClient) { }
   agregarVenta(venta: any): Observable<any> {
-    return this.http.post<any>(this.URL, venta);
+    return this.http.post<any>(`${this.URL}/nueva`, venta);
   }
 
 }
